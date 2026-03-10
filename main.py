e13e9d317f23a25abeddd564eae2d95bcd1949ad
isa <isaguvi2611@gmail.com>
2026-03-10 12:01:25 -0500

    Agregar menu

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ print('''
     Bienvenidos a AromaCampus
     ***********************************
 ''')
+print('''Que deseas hacer?
+    1. Ver menu
+    2. Agregar nuevo cafe
+    3. Salir
+    ''')
+op=int(input('Escriba su opcion'))
+match op:
+    case 1:
+        print('''
+        1. Latte       $7.000
+        2. Capuccino   $8.000
+        3. Americano   $9.500
+        ''')
