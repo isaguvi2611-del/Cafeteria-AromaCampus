6c1a76a2fb4f32bfdff26bc48387a663bb997d01
isa <isaguvi2611@gmail.com>
2026-03-10 12:08:37 -0500

    Agregar cafe

--- main.py
+++ main.py
@@ -16,3 +16,14 @@ match op:
         2. Capuccino   $8.000
         3. Americano   $9.500
         ''')
+    case 2:
+        print('''Desea agregar un cafe
+        1. si
+        2. No
+        ''')
+        select=int(input('Agregue respuesta'))
+        match select:
+            case 1:
+                cafe=input('Ingrese el nombre del cafe')
+                precio=int(input('Ingrese el precio del cafe'))
+                print('El cafe se a agregado correctamente:' , cafe, precio)
